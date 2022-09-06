609c92c5276d581be772b07ac5aa59f0a0285c43
Arjan Weurding <arjan@weurding.eu>
2022-09-06 11:26:00 +0200

    Initial project setup

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-1">
+    <div class="container">
+      <RouterLink :to="{ name: 'home' }" class="navbar-brand">
+        GroceryList
+      </RouterLink>
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#top-navbar"
+        aria-controls="top-navbar"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="top-navbar">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <RouterLink :to="{ name: 'home' }" class="nav-link">
+              Home
+            </RouterLink>
+          </li>
+
+          <li class="nav-item">
+            <RouterLink :to="{ name: 'inventory.overview' }" class="nav-link">
+              Inventory
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <RouterView />
+</template>
---
+++ src/router/index.ts
@@ -0,0 +1,39 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/inventory",
+      name: "inventory.overview",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/inventory/IndexView.vue"),
+    },
+    {
+      path: "/inventory/create",
+      name: "inventory.create",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/inventory/CreateView.vue"),
+    },
+    {
+      path: "/inventory/:id/edit",
+      name: "inventory.edit",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/inventory/EditView.vue"),
+    },
+  ],
+});
+
+export default router;
